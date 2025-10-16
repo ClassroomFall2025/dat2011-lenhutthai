c6b66113e2670915ce5a37fcdfb88a9e834a4f69
Le Nhut Thai <exynos.lnt@gmail.com>
2025-10-16 09:23:27 +0700

    -m "Hoàn thành"

---
+++ ASMchucnang.py
@@ -0,0 +1,27 @@
+# Tạo hàm
+def nhap_nhanvien():
+    print("\nChức năng 1: Nhập danh sách nhân viên từ bàn phím")
+
+def xuat_nhanvien():
+    print("\nChức năng 2: Đọc thông tin nhân viên từ file và xuất danh sách nhân viên ra màn hình")
+
+def tim_nhanvien():
+    print("\nChức năng 3: Tìm và hiển thị nhân viên theo mã nhập từ bàn phím")
+
+def xoa_nhanvien():
+    print("\nChức năng 4: Xóa nhân viên theo mã nhập từ bàn phím")
+
+def capnhat_nhanvien():
+    print("\nChức năng 5: Cập nhật thông tin nhân viên theo mã nhập từ bàn phím")
+
+def tim_theoluong():
+    print("\nChức năng 6: Tìm các nhân viên theo khoảng lương nhập")
+
+def sapxep_hoten():
+    print("\nChức năng 7: Sắp xếp nhân viên theo họ tên")
+
+def sapxep_thunhap():
+    print("\nChức năng 8: Sắp xếp nhân viên theo thu nhập")
+
+def top5_thunhap():
+    print("\nChức năng 9: Hiển thị 5 nhân viên có thu nhập cao nhất")
---
+++ ChuNhat.py
@@ -0,0 +1,16 @@
+class ChuNhat:
+    def __init__(self, rong, dai):
+        self.rong = rong
+        self.dai = dai
+    def chuvi(self):
+        return (self.dai + self.rong) * 2
+    def dientich(self):
+        return self.dai * self.rong
+    def xuat(self):
+        print(f'Chieu dai: {self.dai}')
+        print(f'Chieu rong: {self.rong}')
+        print(f'Chu vi: {self.chuvi()}')
+        print(f'Dien tich: {self.dientich()}')
+class HinhVuong(ChuNhat):
+    def __init__(self, rong, dai):
+        super().__init__(rong, dai)
